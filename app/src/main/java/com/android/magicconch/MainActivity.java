2d36f8b0138b0f9b3eb77646e4ee9b726fa1d654
parkyeonsu02 <parkyandyu02@gmail.com>
2023-09-18 20:59:18 +0900

    test

---
+++ app/src/main/java/com/android/magicconch/MainActivity.java
@@ -0,0 +1,25 @@
+package com.android.magicconch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent; // Intent 클래스를 import해야 합니다.
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        }, 5000);
+    }
+
+}
