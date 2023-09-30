1f1b58291db2a6aabd9a27d21d75329956da3249
lala-david <wnswns1946@naver.com>
2023-09-30 20:16:02 +0900

    #3 옷장페이시 시작

--- app/src/main/java/com/android/magicconch/MainPage.java
+++ app/src/main/java/com/android/magicconch/MainPage.java
@@ -2,13 +2,32 @@ package com.android.magicconch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.magicconch.clothpage.MainCloth;
 
 public class MainPage extends AppCompatActivity {
+
+    Button ClothBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        setTitle("메인 페이지");
+
+        // 옷장 페이지 이동 부분
+        ClothBtn = (Button) findViewById(R.id.clothBtn);
+        ClothBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MainCloth.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/android/magicconch/clothpage/MainCloth.java
@@ -0,0 +1,15 @@
+package com.android.magicconch.clothpage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.magicconch.R;
+
+public class MainCloth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_cloth);
+    }
+}
