8a3156e4f2f735e057915d22d70c47b971d2b224
jsb0315 <jsb031597@gmail.com>
2023-11-11 06:29:11 +0900

    연동 테스트

--- app/src/main/java/com/android/magicconch/MainPage.java
+++ app/src/main/java/com/android/magicconch/MainPage.java
@@ -31,6 +31,15 @@ public class MainPage extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.imgBtn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 Login 액티비티로 이동
+                Intent intent = new Intent(MainPage.this, Mypage.class); // Login은 이동하려는 액티비티의 클래스 이름
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
