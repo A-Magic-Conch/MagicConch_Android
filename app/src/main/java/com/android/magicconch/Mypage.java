b7dd424b1e113aae6baa041b7cb71b5c0d378250
parkyeonsu02 <qkryandyu02@gmail.com>
2023-11-19 04:45:31 +0900

    (로그인수정 #2)

--- app/src/main/java/com/android/magicconch/Mypage.java
+++ app/src/main/java/com/android/magicconch/Mypage.java
@@ -1,16 +1,30 @@
 package com.android.magicconch;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Mypage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String name = user.getDisplayName();
+            TextView name1=(TextView) findViewById(R.id.name1);
+            name1.setText(name);
+        } else {
+            Toast.makeText(getApplicationContext(),"로그인해주세요.",Toast.LENGTH_SHORT).show();
+
+        }
+
 
     }
 
