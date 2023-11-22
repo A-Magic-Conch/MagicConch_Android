f65e51e9bbb1769a5d8a7c6ce1e1b670ad851cfa
parkyeonsu02 <qkryandyu02@gmail.com>
2023-11-22 16:06:15 +0900

    수정(#2)

--- app/src/main/java/com/android/magicconch/Mypage.java
+++ app/src/main/java/com/android/magicconch/Mypage.java
@@ -1,7 +1,11 @@
 package com.android.magicconch;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -9,24 +13,27 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Mypage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String name = user.getDisplayName();
-            TextView name1=(TextView) findViewById(R.id.name1);
-            name1.setText(name);
-        } else {
-            Toast.makeText(getApplicationContext(),"로그인해주세요.",Toast.LENGTH_SHORT).show();
+        // 사용자 정보를 SharedPreferences에서 불러와서 표시
+        SharedPreferences preferences = getSharedPreferences("user_info", MODE_PRIVATE);
+        String userEmail = preferences.getString("email", ""); // "email" 키를 사용하여 이메일을 불러옴
 
-        }
+// 사용자 정보를 화면에 표시
+        TextView emailTextView = findViewById(R.id.name1);
+        emailTextView.setText(userEmail);
 
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Mypage.this, MainPage.class); // Login은 이동하려는 액티비티의 클래스 이름
+                startActivity(intent);
+            }
+        });
 
     }
-
 }
 
