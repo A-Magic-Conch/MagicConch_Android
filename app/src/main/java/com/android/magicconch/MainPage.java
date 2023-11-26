e912ef9aef16310c164936a94753aae26efee3ce
parkyeonsu02 <qkryandyu02@gmail.com>
2023-11-26 19:28:04 +0900

    수정(#2)

--- app/src/main/java/com/android/magicconch/MainPage.java
+++ app/src/main/java/com/android/magicconch/MainPage.java
@@ -25,7 +25,7 @@ public class MainPage extends AppCompatActivity {
         findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 버튼 클릭 시 Login 액티비티로 이동
+                // 버튼 클릭 시 mypage
                 Intent intent = new Intent(MainPage.this, Mypage.class); // Login은 이동하려는 액티비티의 클래스 이름
                 startActivity(intent);
             }
