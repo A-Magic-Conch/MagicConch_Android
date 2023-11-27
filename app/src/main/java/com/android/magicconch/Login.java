1890325885479b2e8af066b032fe6bcb7dbf1afa
parkyeonsu02 <qkryandyu02@gmail.com>
2023-11-27 21:48:29 +0900

    #2 메인 페이지 해상도 변경

--- app/src/main/java/com/android/magicconch/Login.java
+++ app/src/main/java/com/android/magicconch/Login.java
@@ -1,13 +1,13 @@
 package com.android.magicconch;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
